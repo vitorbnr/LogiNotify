33d9a250c3376a77aabda6496d9e2769f2d9218e
vitorbnr <vitorbernardotech@gmail.com>
2026-01-06 22:53:45 -0300

    projeto criado

---
+++ src/main/java/com/vitorbnr/LogiNotify/LogiNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.LogiNotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorbnr/LogiNotify/LogiNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.LogiNotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
