aa5a2ca85248c3713fe7fa93e575614c071a1700
vitorbnr <vitorbernardotech@gmail.com>
2026-01-14 19:18:36 -0300

    refactor(config): implementa fluxo de notificação assíncrona e refatora config do RabbitMQ

--- src/main/java/com/vitorbnr/LogiNotify/config/RabbitMQConfig.java
+++ src/main/java/com/vitorbnr/LogiNotify/config/RabbitMQConfig.java
@@ -1,22 +1,52 @@
 package com.vitorbnr.LogiNotify.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String NOME_FILA = "entrega.notificacao";
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     @Bean
-    public Queue criarFila() {
-        return new Queue(NOME_FILA, true);
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(routingKey);
     }
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 }
