a139a7411dca6732022d6790763d49a6adad886f
vitorbnr <vitorbernardotech@gmail.com>
2026-01-14 19:41:08 -0300

    feat(service): integra persistência do banco ao fluxo de mensageria

--- src/main/java/com/vitorbnr/LogiNotify/service/EntregaService.java
+++ src/main/java/com/vitorbnr/LogiNotify/service/EntregaService.java
@@ -1,6 +1,9 @@
 package com.vitorbnr.LogiNotify.service;
 
+import com.vitorbnr.LogiNotify.domain.Entrega;
+import com.vitorbnr.LogiNotify.domain.StatusEntrega;
 import com.vitorbnr.LogiNotify.dto.EntregaDTO;
+import com.vitorbnr.LogiNotify.repository.EntregaRepository;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +15,9 @@ public class EntregaService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private EntregaRepository entregaRepository;
+
     @Value("${rabbitmq.exchange.name}")
     private String exchange;
 
@@ -19,8 +25,25 @@ public class EntregaService {
     private String routingKey;
 
     public void enviarNotificacao(EntregaDTO entregaDTO) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, entregaDTO);
+        Entrega entrega = new Entrega();
+        entrega.setNomeDestinatario(entregaDTO.nomeCliente());
+        entrega.setEndereco(entregaDTO.endereco());
+        entrega.setEmailDestinatario(entregaDTO.email());
+        entrega.setStatus(StatusEntrega.PENDENTE);
 
-        System.out.println("Mensagem enviada para o RabbitMQ: " + entregaDTO);
+        entrega = entregaRepository.save(entrega);
+        System.out.println("Pedido salvo no banco com ID: " + entrega.getId());
+
+        EntregaDTO mensagemFila = new EntregaDTO(
+                entrega.getId(),
+                entregaDTO.idPedido(),
+                entregaDTO.nomeCliente(),
+                entregaDTO.email(),
+                entregaDTO.endereco(),
+                entrega.getStatus().toString()
+        );
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, mensagemFila);
+        System.out.println("Mensagem enviada para a fila: " + mensagemFila);
     }
 }
