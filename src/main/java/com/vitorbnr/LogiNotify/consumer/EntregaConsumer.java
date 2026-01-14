e5fe30f6758c968ff006b5d04fa0f6221fc53f95
vitorbnr <vitorbernardotech@gmail.com>
2026-01-14 19:43:24 -0300

    refactor(consumer): implementa logica no EntregaConsumer para buscar a entrega pelo ID e atualizar status para ENTREGUE

--- src/main/java/com/vitorbnr/LogiNotify/consumer/EntregaConsumer.java
+++ src/main/java/com/vitorbnr/LogiNotify/consumer/EntregaConsumer.java
@@ -1,21 +1,45 @@
 package com.vitorbnr.LogiNotify.consumer;
 
+import com.vitorbnr.LogiNotify.domain.StatusEntrega;
 import com.vitorbnr.LogiNotify.dto.EntregaDTO;
+import com.vitorbnr.LogiNotify.repository.EntregaRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EntregaConsumer {
 
+    @Autowired
+    private EntregaRepository entregaRepository;
+
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void receberNotificacao(@Payload EntregaDTO entregaDTO) {
-        System.out.println("=================================================");
-        System.out.println("NOVA NOTIFICAÇÃO RECEBIDA!");
-        System.out.println("Pedido ID: " + entregaDTO.idPedido());
-        System.out.println("Cliente: " + entregaDTO.nomeCliente());
-        System.out.println("Status: " + entregaDTO.status());
-        System.out.println("=================================================");
+        System.out.println("--- Recebendo mensagem da fila ---");
 
+        if (entregaDTO.id() != null) {
+            entregaRepository.findById(entregaDTO.id()).ifPresentOrElse(
+                    entrega -> {
+                        simularProcessamento();
+
+                        entrega.setStatus(StatusEntrega.ENTREGUE);
+                        entregaRepository.save(entrega);
+
+                        System.out.println("Status atualizado para ENTREGUE no banco de dados (ID: " + entrega.getId() + ")");
+                    },
+                    () -> System.err.println("Erro: Entrega com ID " + entregaDTO.id() + " não encontrada no banco!")
+            );
+        } else {
+            System.err.println("Erro: Mensagem recebida sem ID da entrega!");
+        }
+    }
+
+    private void simularProcessamento() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/vitorbnr/LogiNotify/dto/EntregaDTO.java
+++ src/main/java/com/vitorbnr/LogiNotify/dto/EntregaDTO.java
@@ -1,8 +1,10 @@
 package com.vitorbnr.LogiNotify.dto;
 
 public record EntregaDTO(
+        Long id,
         String idPedido,
         String nomeCliente,
+        String email,
         String endereco,
         String status
 ) {}
