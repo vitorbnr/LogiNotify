629821561a39530d53314e79362a9669ab11be72
vitorbnr <vitorbernardotech@gmail.com>
2026-01-19 20:25:10 -0300

    feat(dto): adiciona anotacoes para garantir integridade

--- src/main/java/com/vitorbnr/LogiNotify/dto/EntregaDTO.java
+++ src/main/java/com/vitorbnr/LogiNotify/dto/EntregaDTO.java
@@ -1,10 +1,23 @@
 package com.vitorbnr.LogiNotify.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record EntregaDTO(
         Long id,
+
+        @NotBlank(message = "O ID do pedido é obrigatório")
         String idPedido,
+
+        @NotBlank(message = "O nome do cliente é obrigatório")
         String nomeCliente,
+
+        @NotBlank(message = "O email é obrigatório")
+        @Email(message = "O formato do email é inválido")
         String email,
+
+        @NotBlank(message = "O endereço de entrega é obrigatório")
         String endereco,
+
         String status
 ) {}
